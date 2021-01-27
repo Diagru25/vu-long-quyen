d41026f51c9f61eb213ca6617b2c3d495931877e
Diagru25 <diagru2510@gmail.com>
2021-01-27 16:54:55 +0700

    bug animation

--- src/packages/shared/layouts/Sidebar/Sidebar.js
+++ src/packages/shared/layouts/Sidebar/Sidebar.js
@@ -2,21 +2,29 @@ import './Sidebar.scss';
 
 import { Menu } from 'adapters/ant-design';
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Sidebar = () => {
-  return (
-    <div className='sidebar'>
-      <Menu style={{ backgroundColor: '#fafafa' }}>
-        <Menu.Item key='1'>
-          <i class='fas fa-home'></i>
-          Dashboard
-        </Menu.Item>
-        <Menu.Item key='2'>
-          <i class='fas fa-cog'></i>
-          Settings
-        </Menu.Item>
-      </Menu>
-    </div>
-  );
+
+    const { isShowSideBar } = useSelector(state => state.menuReducer);
+
+    return (
+        <div className={isShowSideBar ? 'sidebar' : 'sidebar-hidden'}>
+            <ul className="left-menu">
+                <li>
+                    <div className="items">
+                        <i class='fas fa-home'></i>
+                        <span>Inbox</span>
+                    </div>
+                </li>
+                <li>
+                    <div className="items">
+                        <i class='fas fa-cog'></i>
+                        <span>Inbox</span>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    );
 };
 export default Sidebar;
