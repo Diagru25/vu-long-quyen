0b067b718dfe8c89e89b66ff557403c37150d687
Phạm Minh Tiến <phaminhtien2424@gmail.com>
2021-01-27 15:00:52 +0700

    (chore): config vscode alias

--- src/packages/core/helper/services/services.js
+++ src/packages/core/helper/services/services.js
@@ -2,41 +2,39 @@ import firebase from 'helper/firebaseConfig';
 
 const db = firebase.database;
 const studentServices = {
-    getAllStudents: () => {
-        return db.ref('/students').once('value');
-    },
-    addStudent: (student) => {
-       return db.ref('/students').push(student);
-    },
-    updateStudent: (student) => {
-        let {key, ...cloneStudent} = student;
+  getAllStudents: () => {
+    return db.ref('/students').once('value');
+  },
+  addStudent: (student) => {
+    return db.ref('/students').push(student);
+  },
+  updateStudent: (student) => {
+    let { key, ...cloneStudent } = student;
 
-        const curUpdate = db.ref('/students').child(student.key);
-        curUpdate.set(cloneStudent);
-
-    },
-    deleteStudent: (key) => {
-        db.ref('/students').child(key).remove();
-    }
-}
+    const curUpdate = db.ref('/students').child(student.key);
+    curUpdate.set(cloneStudent);
+  },
+  deleteStudent: (key) => {
+    db.ref('/students').child(key).remove();
+  },
+};
 
 const beltServices = {
-    getAllbelts: () => {
-        return db.ref('/belts').once('value');
-    },
-    addBelt: (belt) => {
-       return db.ref('/belts').push(belt);
-    },
-    updateBelt: (belt) => {
-        let {key, ...cloneBelt} = belt;
+  getAllbelts: () => {
+    return db.ref('/belts').once('value');
+  },
+  addBelt: (belt) => {
+    return db.ref('/belts').push(belt);
+  },
+  updateBelt: (belt) => {
+    let { key, ...cloneBelt } = belt;
 
-        const curUpdate = db.ref('/belts').child(belt.key);
-        curUpdate.set(cloneBelt);
+    const curUpdate = db.ref('/belts').child(belt.key);
+    curUpdate.set(cloneBelt);
+  },
+  deleteBelt: (key) => {
+    db.ref('/belts').child(key).remove();
+  },
+};
 
-    },
-    deleteBelt: (key) => {
-        db.ref('/belts').child(key).remove();
-    }
-}
-
-export {studentServices, beltServices};
+export { studentServices, beltServices };
